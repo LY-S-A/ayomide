fe6be43db34f3a037e4c7e6af4b8bde301e8a722
lysa.waitlist <Lysauniverse30@gmail.com>
2026-09-22 17:39:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Notes from "./pages/Notes";
+import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/notes" element={<Notes />} />
+
+        <Route
+          path="/projects/:projectId"
+          element={<ProjectDetails />}
+        />
       </Routes>
     </BrowserRouter>
   );
